408f3f4848d9d8ad047b52687082c9b347fcf649
gilmab <86659763+gilmab@users.noreply.github.com>
2023-02-07 16:32:28 +0100

    mod

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('','') ;
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
